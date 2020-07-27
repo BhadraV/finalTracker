c08c2cc09d886dbf4e72b66b018f0466df6284ad
User <sreebhadrav1999@gmail.com>
2020-07-27 10:29:41 +0530

    final

--- app/src/main/java/com/example/tracker/Splash.java
+++ app/src/main/java/com/example/tracker/Splash.java
@@ -2,6 +2,7 @@ package com.example.tracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +11,16 @@ public class Splash  extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,MainActivity.class));
+                finish();
+            }
+        }, 1000);
 
-        startActivity(new Intent(Splash.this,MainActivity.class));
+
     }
 }
